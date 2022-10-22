ebab57d84084d99b91286159de389089e8085ba3
Dima <dimacheg06@gmail.com>
2022-10-22 23:30:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+for(let i=1; i<100; i++){
+    console.log(i);
+    if(i == 54){
+        break
+    }
+}
+console.log("______________________");
+
+
+console.log("next exercise");
+console.log("Вывести числа от 1 до 50")
+for(let i=1; i<51; i++){
+    console.log(i);
+}
+console.log("______________________");
+
+
+console.log("next exercise");
+console.log("Вывести числа от 11 до 33")
+for(let i=11; i<34; i++){
+    console.log(i);
+}
+console.log("______________________");
+
+
+console.log("next exercise");
+console.log("Вывести чётные числа от 1 до 100")
+for(let i=2; i<101; i=i+2){
+    console.log("", i);
+}
+console.log("______________________");
+
+
+console.log("next exercise");
+let sum=0
+for(let i=1; i<101; i++){
+    sum=sum+i;
+}
+console.log("Сумма чисел от 1 до 100: ", sum);
